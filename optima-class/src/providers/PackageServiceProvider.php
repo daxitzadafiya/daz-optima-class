adab1f083b14343ebd522a4137fb183f9299e9cd
DaxitZadafiya <daxitzadafiya.work@gmail.com>
2024-11-28 17:23:30 +0530

    first commit

---
+++ optima-class/src/providers/PackageServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Daz\OptimaClass;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register package services
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/package.php' => config_path('package.php'),
+        ]);
+    }
+}
