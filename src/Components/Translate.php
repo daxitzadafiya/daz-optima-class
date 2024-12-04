c243ec25ebf699a044b4eb06785ea49d4bd66766
DaxitZadafiya <daxitzadafiya.work@gmail.com>
2024-12-04 17:09:38 +0530

    made some Improvements

--- src/Components/Translate.php
+++ src/Components/Translate.php
@@ -2,9 +2,82 @@
 
 namespace Daz\OptimaClass\Components;
 
+use Illuminate\Support\Facades\File;
+
 class Translate
 { 
-    public static function t($str){
+    public static function t($str)
+    {
        return __('app.' . strtolower($str));
+    }
+
+    public static function createDefaultLocale(array $locales = ["en", "es"])
+    {
+        $langBasePath = resource_path('lang');
+
+        // Ensure the base lang directory exists
+        Translate::createDirectory($langBasePath);
+
+        // Create locale directories and files
+        foreach ($locales as $locale) {
+            $localePath = sprintf('%s/%s', $langBasePath, $locale);
+            Translate::createDirectory($localePath);
+
+            $filePath = sprintf('%s/app.php', $localePath);
+            Translate::createFile($filePath);
+        }
+    }
+
+    /**
+     * Create a directory if it doesn't exist.
+     *
+     * @param string $path
+     * @return void
+    */
+    public static function createDirectory(string $path)
+    {
+        if (!File::exists($path)) {
+            if (!File::makeDirectory($path, 0755, true)) {
+                return "Failed to create directory: $path";
+            }
+        }
+    }
+
+    /**
+     * Create a file with default content if it doesn't exist.
+     *
+     * @param string $filePath
+     * @return void
+    */
+    public static function createFile(string $filePath)
+    {
+        if (!File::exists($filePath)) {
+            if (file_put_contents($filePath, self::defaultFileContent()) === false) {
+                return "Failed to create file: $filePath";
+            }
+        }
+    }
+
+    /**
+     * Get the default content for the locale files.
+     *
+     * @return string
+    */
+    public static function defaultFileContent(): string
+    {
+        return <<<EOT
+            <?php
+
+            use Daz\OptimaClass\Helpers\Cms;
+
+            \$translations = Cms::getTranslations();
+            \$returnData = [];
+
+            foreach (\$translations as \$translation) {
+                \$returnData[strtolower(\$translation['key'])] = \$translation['value'];
+            }
+
+            return \$returnData;
+        EOT;
     }
 }
