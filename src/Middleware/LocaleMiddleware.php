088f7c33440949f5dcac3534f6481c26a2cdd75c
DaxitZadafiya <daxitzadafiya.work@gmail.com>
2024-12-05 14:35:02 +0530

    made some improvements

--- src/Middleware/LocaleMiddleware.php
+++ src/Middleware/LocaleMiddleware.php
@@ -6,6 +6,7 @@ use Closure;
 use Daz\OptimaClass\Helpers\Cms;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 use Symfony\Component\HttpFoundation\Response;
 
 class LocaleMiddleware
@@ -20,6 +21,12 @@ class LocaleMiddleware
         $locale = $request->segment(1);
 
         $availableLocales = Cms::siteLanguages();
+
+        $remove_lang = Config::get('params.remove_lang', []);
+
+        $availableLocales = array_values(array_filter($availableLocales, function($item) use ($remove_lang) {
+            return !in_array((isset($item['key']) && !empty($item['key']) ? strtolower($item['key']) : $item), $remove_lang);
+        }));
 
         if(in_array($locale, $availableLocales)) {
             App::setLocale($locale);
